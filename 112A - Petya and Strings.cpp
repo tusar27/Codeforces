75d23f9922fed0f84498a2cb3326456f14037826
tusar27 <79058139+tusar27@users.noreply.github.com>
2021-02-15 01:16:05 +0600

    cf-solution

---
+++ 112A - Petya and Strings.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  char A[101],B[101];
+   int i=0;
+   cin>>A>>B;
+
+  int len = strlen(A);
+  for(int i=0; i<len; i++)
+  {
+     if(isupper(A[i]) )
+     {
+         if(A[i] == B[i]  ) ;
+     }
+     else
+     {
+         if(A[i] == B[i]);
+     }
+
+  }
+}
+
